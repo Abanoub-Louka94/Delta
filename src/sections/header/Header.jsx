6609cf098b3f096106e3655b9dada494e3cd127d
Abanoub-Louka <abanoub.louka94@gmail.com>
2025-09-30 21:41:30 +0300

    adding files

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,7 +1,8 @@
+import { motion } from "motion/react";
 import NavBar from "../../Components/navbar/NavBar";
 import Button from "../../Components/button/Button";
 import Icon from "../../Components/icon/Icon";
-import { path, CardData } from "../header/header-js/header";
+import { path, CardData, container, item } from "../header/header-js/header";
 import HeaderCard from "../../Components/header-card/HeaderCard";
 export default function Header() {
 	return (
@@ -9,13 +10,20 @@ export default function Header() {
 			<section className="bg w-full h-full bg-[url(/src/assets/header/header.jpeg)] centered-background">
 				<section className="w-full h-screen bg-gradient-to-b from-black/50 via-black/30 to-black/80 flex  justify-center items-center">
 					<NavBar />
-					<section className="relative">
-						<section className="mb-6 w-fit mx-auto">
+
+					<motion.section
+						className="relative"
+						variants={container}
+						initial="hidden"
+						animate="show"
+					>
+						<motion.section className="mb-6 w-fit mx-auto" variants={item}>
 							<span className="text-sm md:text-base primary-color tracking-[3px] uppercase w-fit py-3 px-6 rounded-full block bg-cyan-500/30 backdrop-blur-sm ">
 								dubai's premier furniture atelier
 							</span>
-						</section>
-						<section className="mb-6 text-center">
+						</motion.section>
+
+						<motion.section className="mb-6 text-center" variants={item}>
 							<h1 className="text-2xl sm:text-4xl md:text-6xl">
 								<span className="block text-white capitalize">
 									where luxury
@@ -24,35 +32,41 @@ export default function Header() {
 									meets craftsmanship
 								</span>
 							</h1>
-						</section>
-						<section className="mb-6 text-center">
+						</motion.section>
+						<motion.section className="mb-6 text-center" variants={item}>
 							<p className="text-white/60 text-sm md:text-xl max-w-2xl">
 								bespoke furniture manufacturing that transforms your vision into
 								timeless pieces of art
 							</p>
-						</section>
-						<section>
+						</motion.section>
+						<motion.section variants={item}>
 							<section className="primary-bg-color px-10 py-5 shadow-lg shadow-cyan-300/25 flex w-fit mx-auto">
 								<Button>
 									<Icon path={path.path_4} color={path.color_black} />
 									<span className="font-semibold">Get a Qute</span>
 								</Button>
 							</section>
-						</section>
-						<section className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
+						</motion.section>
+						<motion.section
+							className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8"
+							variants={item}
+						>
 							{CardData.map((card) => (
 								<HeaderCard data={card} key={card.description} />
 							))}
-						</section>
-						<section className="animate-bounce absolute -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-[-120px] flex items-center column-flex">
+						</motion.section>
+						<motion.section
+							className="animate-bounce absolute -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-[-120px] flex items-center column-flex"
+							variants={item}
+						>
 							<span className="block uppercase text-white text-xs tracking-[2px]">
 								scroll
 							</span>
 							<span className="block">
 								<Icon path={path.path_6} color={path.color_white} />
 							</span>
-						</section>
-					</section>
+						</motion.section>
+					</motion.section>
 				</section>
 			</section>
 		</header>
--- src/sections/header/header-js/header.js
+++ src/sections/header/header-js/header.js
@@ -1,3 +1,5 @@
+import { delay } from "motion";
+
 export const path = {
 	path_4:
 		"m40-40 78-268q-19-41-28.5-84T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80q-45 0-88-9.5T308-118L40-40Zm118-118 128-38q14-4 28.5-3t27.5 7q32 16 67 24t71 8q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 36 8 71t24 67q7 13 7.5 27.5T196-286l-38 128Zm282-162h80v-120h120v-80H520v-120h-80v120H320v80h120v120Zm39-159Z",
@@ -27,3 +29,19 @@ export const CardData = [
 		description: "project delivered",
 	},
 ];
+
+export const container = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			duration: 3,
+			staggerChildren: 0.3,
+		},
+	},
+};
+
+export const item = {
+	hidden: { y: 50, opacity: 0 },
+	show: { y: 0, opacity: 1 },
+};
