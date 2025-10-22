6e1efa1cb3f1a06e69e571df4c3a7da40edccddf
Abanoub-Louka <abanoub.louka94@gmail.com>
2025-10-23 01:16:24 +0300

    fished link sections

--- src/Components/navbar/desk-top-links/DeskTopLinks.jsx
+++ src/Components/navbar/desk-top-links/DeskTopLinks.jsx
@@ -1,3 +1,5 @@
+import { motion } from "motion/react";
+
 export default function DeskTopLinks({ scroll, activeSection, navLinks }) {
 	return (
 		<section className="hidden md:block">
@@ -18,14 +20,21 @@ function List({ item, scroll, activeSection }) {
 	return (
 		<li className="relative">
 			<a
-				className="capitalize text-white group-hover:primary-color"
+				className="capitalize text-white "
 				href="#"
-				onClick={() => scroll(item.id, item.ref)}
+				onClick={(e) => {
+					e.preventDefault();
+					scroll(item.ref, item.id);
+				}}
 			>
 				{item.id}
 			</a>
 			{activeSection === item.id && (
-				<span className="absolute left-0 my-transition bottom-[-4px] h-[2px] primary-bg-color w-full"></span>
+				<motion.span
+					layoutId="activeSection"
+					className="absolute left-0 bottom-[-4px] h-[2px] primary-bg-color w-full"
+					transition={{ type: "spring", stiffness: 200, damping: 15 }}
+				></motion.span>
 			)}
 		</li>
 	);
